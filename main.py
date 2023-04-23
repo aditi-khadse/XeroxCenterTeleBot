32d68cdd8631e934222a90760ec07e2738be8ca4
Aditi Khadse <129103089+aditi-khadse@users.noreply.github.com>
2023-04-24 02:52:23 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -14,62 +14,52 @@ updater = Updater("6146724476:AAF99tbfYU50gMknpDsJEmJaWGSLtNfdyR8", use_context=
 dispatcher = updater.dispatcher
 
 def start(update, context):
-    update.message.reply_text("Hello! Welcome to Xerox Corner. Here, you can view our inventory and place orders. Type /help to see the available commands.")
+    update.message.reply_text("Welcome to OnTips Xerox Printing Bot. Type /help to get help")
     
-def help(update,context):
-    update.message.reply_text("""
-    Here are the available commands:
-    \n/inventory - View the store's inventory
-    \n/order - Place an order
-    \n/upload -
-    \n/contact - Contact information
-     """)
+def help(update, context):
+    update.message.reply_text(
+    """
+    /start - Start the bot
+    /help - Help
+    /details - Details of Xerox printing
+    /upload - Upload files
+    /payments - Payments
+    /contact - Contact Us
+    """
+    )
     
-def inventory(update, context):
-    update.message.reply_text('''/
-    Here is our inventory:
-    Print type - 
-    1) Black and white print
-    2) Color print
-    \nPage type-
-    1) One side
-    2) Both sides
-    \nCompile by-
-    1) Stapler
-    2) Punching Machine
-    3) Spiral Binding
-    ''')
-
-def upload(update, context):
-        update.message.reply_text('''
-        Upload your files by filling this form: https://forms.gle/jJZLdnSesxgNJQSm9''')
-                                  
-def order(update, context):
-        update.message.reply_text('''
-        Enter your order details as follows:
-        \nPrint Type
-        \nPage Type
-        \nCompile by
-        \n
-        \nExample: Black and white, one side, punching machine
-        ''')
-
+def details(update, context):
+    update.message.reply_text(
+    """Details of Xerox printing
+    
+    1) Normal A4 - Rs. 2.00/page
+    2) Color A4 - Rs. 5.00/page
+    3) Glossy A4 - Rs. 10.00/page
+    4) Instant prints - Rs. 5.00 additional cost
+    """
+    )
+    
+# def upload(update, context):
+#     update.message.reply_text()
+    
+# def payments(update, context):
+#     update.message.reply_text()
+    
 def contact(update, context):
-    update.message.reply_text("You can contact on the official mail id: \nWhatsapp us on:")
-
-def handle_message(update, context):
-    update.message.reply_text(f"You said {update.message.text}, use the commands using /")
-
-disp.add_handler(telegram.ext.CommandHandler('start',start))
-disp.add_handler(telegram.ext.CommandHandler('help',help))
-disp.add_handler(telegram.ext.CommandHandler('inventory',inventory))
-disp.add_handler(telegram.ext.CommandHandler('order',order))
-disp.add_handler(telegram.ext.CommandHandler('upload',upload))
-disp.add_handler(telegram.ext.CommandHandler('contact',contact))
-disp.add_handler(telegram.ext.MessageHandler(telegram.ext.Filters.text, handle_message))
-
-updater.start_polling()
-updater.idle()
+    update.message.reply_text(
+    """Contact Us via email or phone
+    
+    Email: ontips@gmxil.com
+    Ph. No.: 9999999999
+    """
+    )
+    
+dispatcher.add_handler(telegram.ext.CommandHandler("start", start))
+dispatcher.add_handler(telegram.ext.CommandHandler("help", help))
+dispatcher.add_handler(telegram.ext.CommandHandler("details", details))
+# dispatcher.add_handler(telegram.ext.CommandHandler("upload", upload))
+# dispatcher.add_handler(telegram.ext.CommandHandler("payments", payments))
+dispatcher.add_handler(telegram.ext.CommandHandler("contact", contact))
 
 
 updater.start_polling()
